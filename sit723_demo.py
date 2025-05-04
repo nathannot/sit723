c65339ea10ab8af86d1e3b2a3b7bb2d614b02945
Nathan <nathannotaras@gmail.com>
2025-05-04 15:51:25 +1000

    Update sit723_demo.py

--- sit723_demo.py
+++ sit723_demo.py
@@ -1,3 +1,21 @@
+yf_data.YfData.user_agent_headers = {
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9",
+    "origin": "https://finance.yahoo.com",
+    "referer": "https://finance.yahoo.com/quote/AAPL",
+    "sec-ch-ua": '"Not(A:Brand";v="99", "Chromium";v="133", "Google Chrome";v="133"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "user-agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/133.0.0.0 Safari/537.36"
+    ),
+}
+
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
